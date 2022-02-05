1456c977cf392f4c8a9fdc5ddc160a3ff8653fe8
dannpr <dannba0@gmail.com>
2022-02-05 15:02:51 +0100

    code algorand

---
+++ config/config.js
@@ -0,0 +1,24 @@
+// config.js
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({
+   path: path.resolve(__dirname, process.env.NODE_ENV +'.env')
+}); 
+
+
+module.exports = {
+    
+    // Algod Instance
+    ALGOD_TOKEN:process.env.ALGOD_TOKEN,
+    ALGOD_SERVER:process.env.ALGOD_SERVER,
+    ALGOD_PORT:process.env.ALGOD_PORT,
+    FAUCET_ACCOUNT:process.env.FAUCET_ACCOUNT
+
+    // // Sender Account
+    // SENDER_MNEMONIC:process.env.SENDER_MNEMONIC,
+
+    // // Receiver
+    // RECEIVER_MNEMONIC:process.env.RECEIVER_MNEMONIC
+
+}
